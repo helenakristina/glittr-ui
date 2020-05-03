8c5af7736abda19647d814e261404dd460ceabcf
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-05-03 11:56:20 -0700

    add login form

--- src/App.js
+++ src/App.js
@@ -7,22 +7,24 @@ import {
 } from "react-router-dom";
 import Home from './pages/home/index.tsx'
 import Signup from './pages/signup/index.tsx'
+import Login from './pages/login/index.tsx'
 
 export default function HomePage() {
   return (
     <Router>
       <div>
-           <div><Link to="/">Home</Link></div> 
-           <div><Link to="/signup">Signup</Link></div>
+        <div><Link to="/">Home</Link></div>
+        <div><Link to="/signup">Signup</Link></div>
+        <div><Link to="/login">Login</Link></div>
         <Switch>
           <Route exact path="/" component={Home}>
           </Route>
           <Route path="/signup" component={Signup}>
+          </Route>
+          <Route path="/login" component={Login}>
           </Route>
         </Switch>
       </div>
     </Router>
   );
 }
-
-
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import styled from 'styled-components';
+
+const Form = styled.form`
+display: flex;
+flex-direction: column;
+width: 30%;
+margin: 0 auto;
+`;
+
+const Login = () => {
+  const [creds, setCreds] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e: any) => {
+    setCreds({
+      ...creds,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={creds.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={creds.password}
+          onChange={handleChange}
+        />
+        <button>submit</button>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
