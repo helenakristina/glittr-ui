c4213066468c0ce7acee65e2b1d95dde09e1cec5
olsonso <olson1sonja@gmail.com>
2020-05-03 11:16:03 -0700

    Add stripe config

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import Home from './pages/home/index.tsx'
-import Signup from './pages/signup/index.tsx'
+import Home from './pages/home/index'
+import Signup from './pages/signup/index'
+import Donate from './pages/donate/index'
 
 export default function HomePage() {
   return (
@@ -14,10 +15,13 @@ export default function HomePage() {
       <div>
            <div><Link to="/">Home</Link></div> 
            <div><Link to="/signup">Signup</Link></div>
+           <div><Link to="/donate">Donate Now</Link></div>
         <Switch>
           <Route exact path="/" component={Home}>
           </Route>
           <Route path="/signup" component={Signup}>
+          </Route>
+          <Route path="/donate" component={Donate}>
           </Route>
         </Switch>
       </div>
---
+++ src/pages/donate/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Stripe from '../../components/index'
+
+const Donate = () => {
+  return (
+
+        <Stripe />
+ 
+  )
+}
+
+export default Donate;
