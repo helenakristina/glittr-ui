33237839d2105a2a966b7e9a8b6332df2e9c9b07
olsonso <olson1sonja@gmail.com>
2020-05-03 21:23:18 -0700

    Add scroll spy to home

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 
 import Home from './pages/home/index'
@@ -16,11 +15,6 @@ export default function HomePage() {
   return (
     <Router>
       <div>
-        <div><Link to="/">Home</Link></div>
-        <div><Link to="/signup">Signup</Link></div>
-        <div><Link to="/donate">Donate Now</Link></div>
-        <div><Link to="/login">Login</Link></div>
-        <div><Link to="/dashboard">Dashboard</Link></div>
         <Switch>
           <Route exact path="/" component={Home}></Route>
           <Route path="/signup" component={Signup}></Route>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,23 +1,67 @@
 import React from 'react';
-import styled from 'styled-components'
+import Scrollspy from 'react-scrollspy';
+import styled from 'styled-components';
 
-const Header = styled.h2`
-background-color: #282c34;
-min-height: 100vh;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-font-size: calc(10px + 2vmin);
-color: white;
-`
+const Section = styled.section`
+	background-color: #282c34;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	font-size: calc(10px + 2vmin);
+	color: white;
+`;
+
+const Nav = styled.div`
+	position: sticky;
+	top: 0;
+	background: #fff;
+	overflow: auto;
+	white-space: nowrap;
+	display: flex;
+`;
+const NavItem = styled.div`
+	display: inline-flex;
+	margin: 0;
+	padding: 0;
+`;
+const NavItemLink = styled.a`
+	padding: 1rem;
+	color: #5500f0;
+	text-decoration: none;
+	transition: background 0.3s ease-in;
+	&:hover {
+		color: red;
+	}
+`;
 
 const Home = () => {
-    return (
-        <div>
-          <Header>Glittr</Header>
-        </div>
-    )
-}
+	return (
+		<>
+			<Nav>
+				<Scrollspy items={['section-1', 'section-2', 'section-3']} currentClassName="is-current">
+					<NavItem>
+						<NavItemLink href="#home">Home</NavItemLink>
+					</NavItem>
+					<NavItem>
+						<NavItemLink href="#about">About</NavItemLink>
+					</NavItem>
+					<NavItem>
+						<NavItemLink href="#parents">Parents</NavItemLink>
+					</NavItem>
+				</Scrollspy>
+			</Nav>
+			<div>
+				<Section id="home">
+					<h1>It's time to have fun!</h1>
+					<div> Dance, draw, act, and play</div>
+				</Section>
+				<Section id="about">About</Section>
+				<Section id="parents">Parents</Section>
+			</div>
+		</>
+	);
+};
 
 export default Home;
